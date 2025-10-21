f99b0f53f78a9ba5d99d333e16083a87536a7f41
Khania Puji Auliya <khaniapujia@gmail.com>
2025-10-21 11:45:57 +0700

    Percobaan 2

--- jobsheet7/SiakadWhile15.java
+++ jobsheet7/SiakadWhile15.java
@@ -21,7 +21,20 @@ public class SiakadWhile15 {
 
             if (nilai > 80 && nilai <= 100) {
                 System.out.println("Nilai mahasiswa ke-" + (i + 1) + "adalah A");
+            } else if (nilai > 73 && nilai <= 80) {
+                System.out.println("Nilai Mahasiswa ke-" + (i + 1) + "adalah B+");
+            } else if (nilai > 65 && nilai <= 73) {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + "adalah B");
+            } else if (nilai > 60 && nilai <= 65) {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + "adalah C+");
+            } else if (nilai > 50 && nilai <= 60) {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + "adalah C");
+            } else if (nilai > 39 && nilai <= 50) {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + "adalah D");
+            } else {
+                System.out.println("Nilai mahasiswa ke-" + (i + 1) + "adalah E");
             }
+            i++;
         }
     }
 
