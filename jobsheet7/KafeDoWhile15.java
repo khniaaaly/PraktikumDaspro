7f698f8bec08c59fec0f91f7fc96320de0dee998
Khania Puji Auliya <khaniapujia@gmail.com>
2025-10-21 19:52:21 +0700

    Percobaan 3

---
+++ jobsheet7/KafeDoWhile15.java
@@ -0,0 +1,32 @@
+package jobsheet7;
+
+import java.util.Scanner;
+
+public class KafeDoWhile15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int kopi, teh, roti, totalHarga;
+        String namaPelanggan;
+        int hargaKopi = 12000, hargaTeh = 7000, hargaRoti = 20000;
+
+        do {
+            System.out.print("Masukkan nama pelanggan (ketik 'batal' untuk keluar): ");
+            namaPelanggan = sc.nextLine();
+            if (namaPelanggan.equalsIgnoreCase("batal")) {
+                System.out.println("Transaksi dibatalkan.");
+                break;
+            }
+            System.out.print("Jumlah kopi\t: ");
+            kopi = sc.nextInt();
+            System.out.print("Jumlah teh\t: ");
+            teh = sc.nextInt();
+            System.out.print("Jumlah roti\t: ");
+            roti = sc.nextInt();
+
+            totalHarga = (kopi * hargaKopi) + (teh * hargaTeh) + (roti * hargaRoti);
+            System.out.println("Total yang harus dibayar: Rp " + totalHarga);
+            sc.nextLine();
+        } while (true);
+    }
+
+}
