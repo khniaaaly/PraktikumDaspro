e7c7380b0532c96141c97fc31be6009931dddc9c
Khania Puji Auliya <khaniapujia@gmail.com>
2025-10-26 14:45:03 +0700

    Modifikasi Percobaan 1

--- jobsheet7/SiakadFor15.java
+++ jobsheet7/SiakadFor15.java
@@ -6,6 +6,7 @@ public class SiakadFor15 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double nilai, tertinggi = 0, terendah = 100;
+        int lulus = 0, tidakLulus = 0;
 
         for (int i = 1; i <= 10; i++) {
             System.out.print("Masukkan nilai mahasiswa ke-" + i + ": ");
@@ -16,9 +17,17 @@ public class SiakadFor15 {
             if (nilai < terendah) {
                 terendah = nilai;
             }
+
+            if (nilai >= 60) {
+                lulus++;
+            } else {
+                tidakLulus++;
+            }
         }
         System.out.println("Nilai Tertinggi: " + tertinggi);
         System.out.println("Nilai Terendah: " + terendah);
+        System.out.println("Jumlah Mahasiswa Lulus \t: " + lulus);
+        System.out.println("Jumlah Mahasiswa Tidak Lulus \t: " + tidakLulus);
     }
 
 }
