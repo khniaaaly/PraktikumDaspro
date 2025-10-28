c6d63e33774f9e8135c1b235e467af35c98a6227
Khania Puji Auliya <khaniapujia@gmail.com>
2025-10-28 11:03:26 +0700

    Percobaan 1

---
+++ jobsheet8/Star15.java
@@ -0,0 +1,18 @@
+package jobsheet8;
+
+import java.util.Scanner;
+
+public class Star15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+
+        System.out.print("Masukkan nilai n = ");
+        n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print("*");
+        }
+    }
+
+}
