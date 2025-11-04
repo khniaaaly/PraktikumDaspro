09db7d9e38a882115b9e58c5e8e91c3c6a73af49
Khania Puji Auliya <khaniapujia@gmail.com>
2025-11-04 11:28:51 +0700

    Modifikasi Percobaan 2

--- jobsheet9/ArrayNilai15.java
+++ jobsheet9/ArrayNilai15.java
@@ -8,14 +8,17 @@ public class ArrayNilai15 {
         int[] nilaiAkhir = new int[10];
         int i;
 
-        for (i = 0; i < 10; i++) {
+        for (i = 0; i < nilaiAkhir.length; i++) {
             System.out.print("Masukkan nilai akhir ke-" + i + " : ");
             nilaiAkhir[i] = sc.nextInt();
         }
 
-        for (i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir ke-" + i + " adalah " + nilaiAkhir[i]);
+        for (i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-" + i + " Lulus!");
+            } else {
+                System.out.println("Mahasiswa ke-" + i + " Tidak Lulus!");
+            }
         }
     }
-
 }
