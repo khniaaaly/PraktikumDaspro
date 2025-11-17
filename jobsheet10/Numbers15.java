10de26aaaaac7ecc0ab644df7f1844934b4958d1
Khania Puji Auliya <khaniapujia@gmail.com>
2025-11-17 20:28:48 +0700

    Modifikasi Percobaan 3

--- jobsheet10/Numbers15.java
+++ jobsheet10/Numbers15.java
@@ -1,11 +1,18 @@
 package jobsheet10;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Numbers15 {
     public static void main(String[] args) {
         int[][] myNumbers = new int[3][];
         myNumbers[0] = new int[5];
         myNumbers[1] = new int[3];
         myNumbers[2] = new int[1];
+
+        for (int i = 0; i < myNumbers.length; i++) {
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }
     }
 
 }
