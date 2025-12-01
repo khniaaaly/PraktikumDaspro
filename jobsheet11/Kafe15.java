1346d9b4f612c0583fd8dea733447177fc819b8c
Khania Puji Auliya <khaniapujia@gmail.com>
2025-12-01 18:21:15 +0700

    Percobaan 2

--- jobsheet11/Kafe15.java
+++ jobsheet11/Kafe15.java
@@ -1,7 +1,13 @@
 package jobsheet11;
 
 public class Kafe15 {
-    public static void Menu() {
+    public static void Menu(String namaPelanggan, boolean isMember) {
+        System.out.println("Selamat Datang, " + namaPelanggan + "!");
+
+        if (isMember) {
+            System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
+        }
+
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappuccino - Rp 20,000");
@@ -14,7 +20,7 @@ public class Kafe15 {
     }
 
     public static void main(String[] args) {
-        Menu();
+        Menu("Andi", true);
     }
 
 }
