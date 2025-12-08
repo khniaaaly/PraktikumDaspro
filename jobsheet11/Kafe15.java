467e3745faa94c3c058d91b8b4bd84f7d30c5e81
Khania Puji Auliya <khaniapujia@gmail.com>
2025-12-08 19:14:26 +0700

    Percobaan 3

--- jobsheet11/Kafe15.java
+++ jobsheet11/Kafe15.java
@@ -1,5 +1,7 @@
 package jobsheet11;
 
+import java.util.Scanner;
+
 public class Kafe15 {
     public static void Menu(String namaPelanggan, boolean isMember, String kodePromo) {
         System.out.println("Selamat Datang, " + namaPelanggan + "!");
@@ -8,18 +10,43 @@ public class Kafe15 {
             System.out.println("Anda adalah member, dapatkan diskon 10% untuk setiap pembelian!");
         }
 
+        if (kodePromo.equalsIgnoreCase("DISKON50")) {
+            System.out.println("Selamat! Anda mendapatkan DISKON 50%!");
+        } else if (kodePromo.equalsIgnoreCase("DISKON30")) {
+            System.out.println("Selamat! Anda mendapatkan DISKON 30%!");
+        } else if (!kodePromo.isEmpty()) {
+            System.out.println("Kode promo invalid atau tidak berlaku.");
+        }
+
         System.out.println("===== MENU RESTO KAFE =====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappuccino - Rp 20,000");
         System.out.println("3. Latte - Rp 22,000");
-        System.out.println("4. Teh Tarik - 12,000");
-        System.out.println("5. Roti Bakar - 10,000");
-        System.out.println("6. Mie Goreng - 18,000");
+        System.out.println("4. Teh Tarik - Rp 12,000");
+        System.out.println("5. Roti Bakar - Rp 10,000");
+        System.out.println("6. Mie Goreng - Rp 18,000");
         System.out.println("===========================");
         System.out.println("Silahkan pilih menu yang anda inginkan.");
     }
 
+    public static int hitungTotalHarga(int pilihanMenu, int banyakItem) {
+        int[] hargaItems = { 15000, 20000, 22000, 12000, 10000, 18000 };
+
+        int hargaTotal = hargaItems[pilihanMenu - 1] * banyakItem;
+        return hargaTotal;
+    }
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
         Menu("Budi", true, "DISKON30");
+
+        System.out.print("\nMasukkan nomor menu yang ingin Anda pesan: ");
+        int pilihanMenu = sc.nextInt();
+        System.out.print("Masukkan jumlah item yang ingin dipesan: ");
+        int banyakItem = sc.nextInt();
+
+        int totalHarga = hitungTotalHarga(pilihanMenu, banyakItem);
+        System.out.println("Total harga untuk pesanan Anda: Rp" + totalHarga);
     }
 }
