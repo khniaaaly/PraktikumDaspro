6039d442bfe874bd10894bca875812fce490099c
Khania Puji Auliya <khaniapujia@gmail.com>
2025-12-08 20:36:19 +0700

    Tugas 1

---
+++ jobsheet11/Tugas1.java
@@ -0,0 +1,27 @@
+package jobsheet11;
+
+import java.util.Scanner;
+
+public class Tugas1 {
+
+    public static int hitungVolume(int sisi) {
+        return sisi * sisi * sisi;
+    }
+
+    public static int hitungLuasPermukaan(int sisi) {
+        return 6 * sisi * sisi;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan panjang sisi kubus: ");
+        int sisi = sc.nextInt();
+
+        int volume = hitungVolume(sisi);
+        int luasPermukaan = hitungLuasPermukaan(sisi);
+
+        System.out.println("Volume kubus: " + volume + " cm");
+        System.out.println("Luas permukaan kubus: " + luasPermukaan + " cm²");
+    }
+}
