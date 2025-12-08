b3619dc3a5d73ac1652907ea5deeed3c0108d76c
Khania Puji Auliya <khaniapujia@gmail.com>
2025-12-08 20:12:21 +0700

    Modifikasi Percobaan 4

--- jobsheet11/PengunjungCafe15.java
+++ jobsheet11/PengunjungCafe15.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class PengunjungCafe15 {
     static void daftarPengunjung(String... namaPengunjung) {
         System.out.println("Daftar Nama Pengunjung");
-        for (int i = 0; i < namaPengunjung.length; i++) {
-            System.out.println("- " + namaPengunjung[i]);
+        for (String nama : namaPengunjung) {
+            System.out.println("- " + nama);
         }
     }
 
